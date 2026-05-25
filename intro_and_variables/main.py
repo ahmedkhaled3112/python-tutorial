41973a24b33dd16aa71900972cc656bd44466516
ahmedkhaled3112 <ahmedklose3112@gmail.com>
2026-05-25 18:52:25 +0300

    feat: add introductory python program demonstrating variables and conditional logic

---
+++ intro_and_variables/main.py
@@ -0,0 +1,31 @@
+# this is my first program with python
+print("Hello, World!\nThis is my first program")
+
+# The Variable is a container for a value 
+# The variable behaves as if it was the value it contains
+
+# String
+first_name = "ahmed"
+last_name = "khaled"
+
+print(first_name, last_name)
+print(f"Hello, {first_name} {last_name}")
+print(f"Your full name is {first_name} {last_name}")
+
+# Integers 
+age = 26 
+print(f"you are {age} years old")
+
+# float
+gpa = 3.6
+print(f"your GPA is {gpa}")
+
+# Boolean "True or False"
+is_student = True 
+print("Are You Student? :" ,is_student)
+
+# if & else
+if is_student:
+    print("You Are Student")
+else:
+    print("You Are Graduate")
